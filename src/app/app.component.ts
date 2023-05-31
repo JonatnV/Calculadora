3d2190fe5ae9ab9524a97c9dff3141358dfebe7b
JonatnVgit commit -m lanzamientoHTMLgit status -sgit config --global user.name JonatnV <tatanversua@gmail.com>
2023-05-31 07:45:10 -0500

    Operaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,59 @@ export class AppComponent {
   title = 'Calculadora';
   
   COperacion: string ="";
-  CCalculo: string=""
+  CResultado: string ="";
+  CCalculo:string ="";
   agregarNum(numero: number) {
   
     this.COperacion +=numero.toString();
+    this.CCalculo +=numero.toString();
   }
   borrar(){
     this.COperacion ="";
+    this.CResultado ="";
+    this.CCalculo ="";
   }
-  
+  agregarSum( ){
+    this.COperacion+="+";
+    this.CCalculo+="+"
+  }
+  agregarMen(){
+    this.COperacion+="-";
+    this.CCalculo+="-"
+  }
+  agregarMult(){
+    this.COperacion+="X";
+    this.CCalculo+="*"
+  }
+  agregarDiv(){
+    this.COperacion+="÷";
+    this.CCalculo+="/"
+  }
+  abrirPar(){
+    this.COperacion+="(";
+    this.CCalculo+="("
+  }
+  cerrarPar(){
+    this.COperacion+=")";
+    this.CCalculo+=")"
+  }
+  agregarPot2(){
+    this.COperacion+="^2";
+    this.CCalculo+="**2";
+  }
+  agregarPot(){
+    this.COperacion+="^";
+    this.CCalculo+="**";
+  }
+  agregarPorC(){
+    this.COperacion+="%";
+    this.CCalculo+="/100"
+
+  }
+  operar(){
+    
+    this.CResultado= eval(this.CCalculo);
+  }
   
   
 }
